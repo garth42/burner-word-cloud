877df46f3bed2f48e6b57fb86890c33b454402bc
James Brown <jimnig@gmail.com>
2014-06-18 16:34:22 -0600

    Layout tweaks. "One-off" words option.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,7 @@
-updateYearSelect();
+$(document).ready(function() {
+	updateYearSelect();
+});
+
 function updateYearSelect() {
 	$('#year').empty();
 	var entity = $('#entity').val();
@@ -15,13 +18,15 @@ function drawWordCloud() {
 	var year = $('#year').val();
 	if (entity === "" || year === "")
 		return;
+
+	$('#draw_word_cloud').attr('value', 'doing the thing...');
+	$('#word_cloud').height(window.innerHeight - $('#footer_wrap').height() - $('#header').height() - 50);
 	
-	var nameAttribute = entity === "event" ? "title" : "name";
 	$.getJSON("http://playaevents.burningman.com/api/0.2/" + year + "/" + entity + "/?callback=?", function(data) {
-		var excluded = ['i', 'of', 'or', 'the', 'n', 'a', 'and', 'an', 'at', 'that', 'la', 'da', 'du', 'de', 'le', 'this', 'be', 'by', 'it', 'for', 'is'];
+		var excluded = ['i', 'of', 'or', 'the', 'n', 'a', 'and', 'an', 'at', 'that', 'la', 'da', 'du', 'de', 'le', 'this', 'be', 'by', 'it', 'for', 'is', 'to', 'in', 'on', 'as', 'do', 'then'];
 		var wordCounts = {};
 		for (var i = 0; i < data.length; i++) {
-			var campNameWords = data[i][nameAttribute]
+			var campNameWords = data[i][entity === "event" ? "title" : "name"]
 				.replace(/[\.,'"-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+0123456789]/g, '')
 				.replace(/\s{2,}/g," ")
 				.split(" ")
@@ -32,39 +37,44 @@ function drawWordCloud() {
 			}
 		}
 
-		var final = [];
-		var biggest = 0;
-		var i = 0;
-		for (var foo in wordCounts) {
-			final[i] = [foo, wordCounts[foo]];
-			i++;
-		}
-		var biggest = printBiggestWord(final);
-
+		var showSingles = $('#show_single_words').is(':checked');
+		var wordCountsArray = makeArrayFromMap(wordCounts, showSingles);
+		var biggest = getBiggestWord(wordCountsArray);
 		WordCloud($('#word_cloud')[0], {
-			list: final,
+			list: wordCountsArray,
 			fontFamily: 'Helvetica, serif',
 			gridSize: 7,
 			clearCanvas: true,
 			weightFactor: function (size) {
-				return Math.log(size) / Math.log(biggest) * (200 - 15) + 15;
+				return Math.log(size) / Math.log(biggest) * (200 - 18) + 18;
 			},
 			rotateRatio: 0.5,
 			minRotation: 1.57079633,
 			maxRotation: 1.57079633,
 			backgroundColor: '#f2f2f2',
-			//minSize: 15
+			shape: 'square',
 		});
+		$('#draw_word_cloud').attr('value', 'do the thing');
 	});
 }
 
-function printBiggestWord(count) {
-	var biggest = 0;
-	for (var i = 0; i < count.length; i++) {
-		if (count[i][1] > biggest)
-		 biggest = count[i][1];
+function makeArrayFromMap(map, showSingles) {
+	var i = 0, array = [];
+	for (var item in map) {
+		if (!showSingles && map[item] === 1)
+			continue;
+		array[i] = [item, map[item]];
+		i++;
 	}
-	console.log('biggest word was ' + biggest);
+	return array;
+}
+
+function getBiggestWord(wordCountsArray) {
+	var biggest = 0;
+	for (var i = 0; i < wordCountsArray.length; i++) {
+		if (wordCountsArray[i][1] > biggest)
+			biggest = wordCountsArray[i][1];
+	}
 	return biggest;
 }
 
