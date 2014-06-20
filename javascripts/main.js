9ea2e880f9214cd1541e3fe76612f7b0882d7648
James Brown <jimnig@gmail.com>
2014-06-19 23:24:40 -0600

    Visual changes. Better regex-ing.

--- javascripts/main.js
+++ javascripts/main.js
@@ -19,17 +19,21 @@ function drawWordCloud() {
 	if (entity === "" || year === "")
 		return;
 
-	$('#draw_word_cloud').attr('value', 'doing the thing...');
-	$('#word_cloud').height(window.innerHeight - $('#footer_wrap').height() - $('#header').height() - 50);
+	$('#draw_word_cloud').attr('value', 'doing...');
+	$('#word_cloud').height(window.innerHeight - $('#footer_wrap').height() - $('#main_content').height() - 40);
 	
 	$.getJSON("http://playaevents.burningman.com/api/0.2/" + year + "/" + entity + "/?callback=?", function(data) {
-		var excluded = ['i', 'of', 'or', 'the', 'n', 'a', 'and', 'an', 'at', 'that', 'la', 'da', 'du', 'de', 'le', 'this', 'be', 'by', 'it', 'for', 'is', 'to', 'in', 'on', 'as', 'do', 'then'];
+		var excluded = ['i', 'of', 'or', 'the', 'n', 'a', 'and',
+						'an', 'at', 'that', 'la', 'da', 'du', 'de',
+						'le', 'this', 'be', 'by', 'it', 'for',
+						'is', 'to', 'in', 'on', 'as', 'do', 'then',
+						'your', 'we', 'got', 'th', 'rd'];
 		var wordCounts = {};
 		for (var i = 0; i < data.length; i++) {
 			var campNameWords = data[i][entity === "event" ? "title" : "name"]
-				.replace(/[\.,'"-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+0123456789]/g, '')
+				.replace(/[\.,'"#!$%\^&\*’”;:{}=_`~()@\+\?><\[\]\+0123456789]/g, '')
 				.replace(/\s{2,}/g," ")
-				.split(" ")
+				.split(/[\s —–\-\/]+/)
 				.map(function(value) { return value.toLowerCase(); })
 				.filter(function(item) { return item.length > 1 && excluded.indexOf(item) === -1; });
 			for (var j = 0; j < campNameWords.length; j++) {
@@ -46,13 +50,19 @@ function drawWordCloud() {
 			gridSize: 7,
 			clearCanvas: true,
 			weightFactor: function (size) {
-				return Math.log(size) / Math.log(biggest) * (200 - 18) + 18;
+				return Math.log(size) / Math.log(biggest) * (220 - 17) + 17;
 			},
 			rotateRatio: 0.5,
 			minRotation: 1.57079633,
 			maxRotation: 1.57079633,
-			backgroundColor: '#f2f2f2',
+			color: function(word, weight, fontSize, distance, theta) {
+				var	colours = ['#FF0000', '#FF8000', '#D4CE19', '#D6CF00', '#C43323', '#ED9015', '#82001E', '#E25822', '#C20A0A', '#9C1919', '#E62600'];
+				return (colours)[Math.floor(Math.random() * colours.length)];
+			},
+			backgroundColor: '#212121',
 			shape: 'square',
+			abortThreshold: 200
+			//hover: function() {window.drawBox}
 		});
 		$('#draw_word_cloud').attr('value', 'do the thing');
 	});
